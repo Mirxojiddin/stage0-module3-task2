7744135fb944644a7419b8b5ee284d679c2e8166
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-01 15:16:15 +0500

    natija uchun

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention copy.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention copy.java
@@ -1,10 +1,15 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
+    
     private static final int ADULT_AGE = 18;
     private int age;
     private int phoneNumber;
     
+    public static void main(String[] args) {
+        System.out.println("salom");
+        // ...
+    }
     void cAllToFriend() {
         //some code
     }
